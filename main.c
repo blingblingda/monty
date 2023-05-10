7582445742f6427f48d7d2b80d73c864e33019a2
Belinda Shan <belindashan126@hotmail.com>
2023-05-10 21:55:59 +0800

    add push and pall

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -16,7 +17,7 @@ typedef struct stack_s
         int n;
         struct stack_s *prev;
         struct stack_s *next;
-} stack_t;
+} stack_c;
 
 /**
  * struct instruction_s - opcode and its function
@@ -29,9 +30,86 @@ typedef struct stack_s
 typedef struct instruction_s
 {
         char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+        void (*f)(stack_c **stack, unsigned int line_number);
 } instruction_t;
 
+
+int is_integer(char *str)
+{
+    int i;
+
+    while (str[i] != '\0')
+    {
+        if (isdigit(str[i]) == 0)
+        {
+            return (-1);
+        }
+        i = i + 1;
+    }
+
+    return (0);
+}
+
+void push(stack_c **stack, __attribute__((unused)) unsigned int line_number)
+{
+    char *token;
+    char *delim = " \n";
+    int integer_check;
+    int num;
+    stack_c *new_node;
+
+    // if (stack == NULL)
+    // {
+    //     fprintf(stderr, "No stack found\n");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    token = strtok(NULL, delim);
+    if (token == NULL)
+    {
+        fprintf(stderr, "Usage: push integer\n");
+        exit(EXIT_FAILURE);      
+    }
+
+    // printf("%s\n",token);
+    integer_check = is_integer(token);
+    if (integer_check == -1)
+    {
+        fprintf(stderr, "Usage: push integer check\n");
+        exit(EXIT_FAILURE);   
+    }
+    else
+    {
+        num = atoi(token);
+        new_node = malloc(sizeof(stack_c));
+        if (new_node == NULL)
+        {
+            fprintf(stderr, "Error: malloc failed\n");
+            exit(EXIT_FAILURE);           
+        }
+        new_node->n = num;
+        new_node->prev = NULL;
+        if (*stack != NULL) //why
+        {
+            new_node->next = *stack;
+            (*stack)->prev = new_node;
+        }
+        *stack = new_node;
+    }
+}
+
+void pall(stack_c **stack, __attribute__((unused)) unsigned int line_number)
+{
+    stack_c *temp;
+
+    temp = *stack;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
+
+}
 
 /**
 * get_instruction_fn - find function to match opcode
@@ -39,7 +117,7 @@ typedef struct instruction_s
 *
 * Return: success pointer to function, opcode not found: NULL
 */
-void (*get_instruction_fn(char *str))(stack_t **stack, unsigned int line_number)
+void (*get_instruction_fn(char *str))(stack_c **stack, unsigned int line_number)
 {
 	instruction_t opcodes[] = {
 		{"push", push},
@@ -76,9 +154,9 @@ void (*get_instruction_fn(char *str))(stack_t **stack, unsigned int line_number)
     size_t bufsize = 0;
     size_t read = 0;
     char *token;
-    char *delim = " \n";
+    char *delim = " \n"; // why?
     unsigned int line_number = 0;
-    stack_t **stack = NULL; //why ????
+    stack_c **stack = NULL;; //why ????
 
     if (argc != 2)
     {
